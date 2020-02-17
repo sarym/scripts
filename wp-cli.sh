cdafed1bf23cbf4b85e18228a3cb5af5f45454ef
sarym <60790135+sarym@users.noreply.github.com>
2020-02-17 11:38:53 -0700

    Update wp-cli.sh

--- wp-cli.sh
+++ wp-cli.sh
@@ -3,4 +3,4 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 php wp-cli.phar --info
 chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp
